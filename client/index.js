97837c8fc63fd333cf074aa48793426620a3c858
Nick Frattaroli <Nafratt@gmail.com>
2020-01-04 09:56:44 -0500

    boilerplatework

--- client/index.js
+++ client/index.js
@@ -7,14 +7,7 @@ import store from "./redux/store";
 import App from "./App";
 
 /**
- * Wrapping our entire app in the react-redux Provider component
- * 
- * The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
- * Since any React component in a React Redux app can be connected,
- * most applications will render a <Provider> at the top level,
- * with the entire app’s component tree inside of it.
- * Normally, you can’t use a connected component unless it is nested inside of a <Provider>.
- * 
+react redux may be used 
  * @see {@link https://react-redux.js.org/api/provider}
  */
 const renderApp = () => render(
