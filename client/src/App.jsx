edfe281f49c86d7a432b71f7642bff25d70ce035
Nick Frattaroli <Nafratt@gmail.com>
2020-01-09 20:46:19 -0500

    possible fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,10 @@ import { Container, } from 'react-bootstrap';
 import {Example,Stuff} from './components/testmodal';
 import './AppStyles.scss'
 import { Footer } from "./components/footer";
-// import Login from "./components/Auth"
+
 
 export default () => <React.Fragment>
 
-  {/* <Login/> */}
   <Container><h1 className="cent">Gruber
   <Example className="shopping" /></h1>
   <Stuff/>
