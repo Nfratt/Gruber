3da65c5c8c97087e05ed023a13bae09f5c68631d
Rachael Clark <rachael.clark@uconn.edu>
2020-01-11 11:15:31 -0500

    stripe payment in checkout modal

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 // const axios = require("axios");
 
-
+//Stripe Payment//  
+const stripe = require("stripe")("sk_test_ZrMs5whpughrXiBk61CFnVyN009OcQ6LD5");
+app.use(require("body-parser").text());
 
 
 
@@ -22,6 +24,26 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 //   API routes 
+
+//Stripe Payment/
+app.post("/charge", async (req, res) => {
+  try {
+    let {status} = await stripe.charges.create({
+      amount: 2000,
+      currency: "usd",
+      description: "Rachael Testing",
+      source: req.body
+    });
+
+    res.json({status});
+  } catch (err) {
+    console.log(err);
+    res.status(500).end();
+  }
+});
+//End Stripe Payment//
+
+
 
 app.use(routes);
 
