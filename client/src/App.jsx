3ff873563058925280e268a53b860f2b26ef6dc7
Rachael Clark <rachael.clark@uconn.edu>
2020-01-11 13:39:15 -0500

    items display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,31 @@ import './AppStyles.scss'
 import { Cart } from "./componants/cart";
 import { Login } from "./componants/login";
 import Navbar from "./componants/nav";
-
-
+import Product from './componants/Product';
+import items from '../src/api/api';
 
 export default () => <React.Fragment>
-  
+
   <Container>
     <h1>Gruber</h1>
-  <Navbar>
-    <Cart />
-    <Login />
+    <Navbar>
+      <Cart />
+      <Login />
     </Navbar>
     <div>items go here</div>
 
-    
+
+    <div className="App">
+
+      <div className="App-products">
+        {items.map(item => (
+          <Product key={item.title} title={item.title} price={item.price} />
+        ))}
+      </div>
+
+    </div>
+
+
   </Container>
 
 </React.Fragment>
