5f5a0541007c99763b7512f6974a98c1f2c43766
Nick Frattaroli <Nafratt@gmail.com>
2020-01-11 09:41:36 -0500

    adding modals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { Container } from 'react-bootstrap';
 import './AppStyles.scss'
 import { Example, Stuff } from "./componants/testmodal";
+import { Checkout,CheckoutBtn } from "./componants/checkout";
+import { Cart, CartBtn } from "./componants/cart";
+import { Login, Loginbtn } from "./componants/login";
 
 export default () => <React.Fragment>
   <Container><h1>Gruber</h1>
   <Example/>
+  <Checkout/>
+  <Cart/>
+  <Login/>
   <Stuff/>
+  <CartBtn/>
+  <CheckoutBtn/>
+  <Loginbtn/>
   </Container>
 
 </React.Fragment>
