59020a64f3fe398ec32282f435cc935067486dc2
Nick Frattaroli <Nafratt@gmail.com>
2020-01-23 21:07:47 -0500

    new logo

--- client/src/componants/nav.jsx
+++ client/src/componants/nav.jsx
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { CartBtn } from "./cart";
 import {  Loginbtn } from "./login";
 import { EditBtn } from "./editAcct"
+import Logo from "./images/Gruberlogo.png";
 
 
 export default function Navbar(props){
     return(
 <Nav className="justify-content-end" id="navbkrd" defaultActiveKey="/home">
+<img className="logo" src={Logo} alt="Gruberlogo logo" />
     <Loginbtn className="navitem"/>
     <CartBtn className="navitem"/>
 </Nav>
