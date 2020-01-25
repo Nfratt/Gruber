d8a9823d3cb25f15fb6682be3238c0e9c9a5aa8f
baconbits1204 <rafaelhermoso1204@gmail.com>
2020-01-25 13:59:54 -0500

    polishing

--- client/src/componants/Product.jsx
+++ client/src/componants/Product.jsx
@@ -9,12 +9,12 @@ function Product({ onAdd, item}) {
         <Card className="Product" style={{ width: '18rem' }}>
   <Card.Img className="product-image" variant="top" src={item.image} />
   <Card.Body>
-    <Card.Title>{item.itemName}</Card.Title>
-    <Card.Text className="product-price">
-    {item.description}...${item.price}
+    <Card.Title className="product-price">{item.itemName}...${item.price}</Card.Title>
+    <Card.Text >
+    {item.description}
     </Card.Text>
   </Card.Body>
-  <Card.Footer>
+  <Card.Footer className="card-footer">
   <Button variant="primary" className="Product-buy-button" onClick={() => {
                 console.log({ item})
                 onAdd(item)
