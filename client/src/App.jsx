d77614c42d3bf69b63095f9f51a8b156e1d0aaf8
Nick Frattaroli <Nafratt@gmail.com>
2020-01-25 10:01:30 -0500

    cleanup and linting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,12 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import "./AppStyles.scss";
 import { Login } from "./componants/login";
 import Navbar from "./componants/nav";
 import Product from "./componants/Product";
-// import items from "../src/api/api";
+
 import Footer from "./componants/footer";
-import Logo from "./componants/images/Gruberlogo.png";
-// import Utils from "./utils/library.js";
+
 import API from './api';
-
-// export default () => <React.Fragment>
-// <div className="background">
-//   <Container className="productcontainer">
-//   <img className="logo" src={Logo} alt="Gruberlogo logo" />
-//     <h3>Get your Groceries on Demand!!</h3>
-//     <Navbar>
-//       <Cart />
-//       <Login />
-//     </Navbar>
-//     <div>items go here</div>
-//     <div className="App">
-//       <div className="App-products">
-//         {items.map(item => (
-//           <Product key={item.title} title={item.title} price={item.price} />
-//         ))}
-//       </div>
-//     </div>
-//   </Container>
-//   <Footer/>
-//   </div>
-
-// </React.Fragment>
-
 
 class App extends React.Component {
   // Setting the initial state of the App component
